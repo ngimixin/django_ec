42a5017952671cc83a8becd18b6e2d520537575d
ngimixin <nagai.hideaki.2022@gmail.com>
2025-11-24 15:15:38 +0900

    docs: ビューのDocstring表現を統一

--- products/views.py
+++ products/views.py
@@ -4,7 +4,7 @@ from .models import Product
 
 
 def product_list(request: HttpRequest) -> HttpResponse:
-    """公開中の商品一覧ページを表示する。"""
+    """公開中の商品一覧ページを表示するビュー。"""
     products = Product.objects.filter(is_active=True).order_by("-created_at")
 
     return render(request, "products/product_list.html", {"products": products})
