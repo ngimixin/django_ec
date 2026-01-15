ac102a01e9a1b31158aa67a68eefc30f8f63abd4
ngimixin <nagai.hideaki.2022@gmail.com>
2026-01-15 23:46:07 +0900

    feat: 管理画面に Order と OrderItem を追加する

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Order, OrderItem
 
 
 @admin.register(Product)
@@ -15,3 +15,39 @@ class ProductAdmin(admin.ModelAdmin):
         "updated_at",
     )
     search_fields = ("name", "sku")
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "name",
+        "email",
+        "phone",
+        "postal_code",
+        "total_amount",
+        "status",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("name", "email", "phone", "postal_code", "address")
+    inlines = [OrderItemInline]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order",
+        "product",
+        "price",
+        "quantity",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("order__name", "product__name")
